bdfe649875dc8cf56479eeaf5488bb5cd128de6b
John Reese <jpreese@mtu.edu>
2017-02-17 00:06:07 -0500

    Add logo callback

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 var $ = require('jquery');
 var getQuote = require('./services/getQuote.js');
+var getLogo = require('./services/getLogo.js');
 
 $(document).ready(function() {
     
     $("#searchControl").click(function() {
         var tickerInputValue = $("#tickerInput").val();
-        getQuote(tickerInputValue, searchTickerCallback);
+        getQuote(tickerInputValue, tickerInfoCallback);
+        getLogo(tickerInputValue, logoCallback)
     });
     
 });
 
-var searchTickerCallback = function(result) {
+var tickerInfoCallback = function(result) {
     $("#companyName").text(result.CompanyName);
     $("#openValue").text(result.OpenAmount);
     $("#highValue").text(result.DayHigh);
     $("#lowValue").text(result.DayLow);
+}
+
+var logoCallback = function(result) {
+    var logoUrl = $(result).find("URL").text();
+    $("#logo").attr("src", logoUrl);
 }
