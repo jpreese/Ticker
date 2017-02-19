7678dda435e077a58415000a38f8a6a6c3b0b3f9
John Reese <jpreese@mtu.edu>
2017-02-18 19:53:52 -0500

    Remove loaders

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,6 @@ var config = {
     filename: 'bundle.js'
   },
   
-  module : {
-    loaders : [
-      {test: /\.json$/, loader: 'json-loader'},
-      {test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'}
-    ]
-  },
-  
   target: 'web'
 };
 
